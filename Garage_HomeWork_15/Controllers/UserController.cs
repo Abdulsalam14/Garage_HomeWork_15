bb365f8d5d376a1cb02538646a4146578f3e8e85
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2023-12-03 23:27:31 +0400

    Add project files.

---
+++ Garage_HomeWork_15/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Garage_HomeWork_15.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Abdulsalam";
+            ViewData["Surname"] = "Abdullayev";
+            ViewData["Age"] = 25;
+            ViewData["Country"] = "Azerbaijan";
+            ViewData["PhoneNumber"]= "+994705641234";
+            return View();
+        }
+    }
+}
